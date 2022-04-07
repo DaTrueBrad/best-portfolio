5c88e209f591cd0fa6e56509c839022020bafd00
Brady Bott <bottbrady@gmail.com>
2022-04-07 13:48:34 -0600

    test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@emotion/react";
 import { themeOptions } from "./themeOptions";
 import { BrowserRouter } from "react-router-dom";
+import {initializeApp} from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const app = initializeApp({
+  apiKey: "AIzaSyA5Ob5i2DaizTlAwbdn00rWl8iZZ7Yvuzg",
+  authDomain: "portfolio-7a1b5.firebaseapp.com",
+  projectId: "portfolio-7a1b5",
+  storageBucket: "portfolio-7a1b5.appspot.com",
+  messagingSenderId: "319732382959",
+  appId: "1:319732382959:web:0546246bf707d03c79ef1a",
+  measurementId: "G-XTYET23CZ3"
+});
+const analytics = getAnalytics(app);
+
+
 
 ReactDOM.render(
   <React.StrictMode>
